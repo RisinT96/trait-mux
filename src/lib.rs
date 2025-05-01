286a76db3712d977682a3edfefefaa776d73b55b
Tal Risin <risint96@gmail.com>
2025-05-01 18:46:56 +0300

    remove analyze stage

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use proc_macro::TokenStream;
 use proc_macro_error::proc_macro_error;
 
 mod trait_mux;
-use crate::trait_mux::analyze;
 use crate::trait_mux::codegen;
 use crate::trait_mux::lower;
 use crate::trait_mux::parse;
@@ -11,8 +10,7 @@ use crate::trait_mux::parse;
 #[proc_macro_error]
 pub fn trait_mux(ts: TokenStream) -> TokenStream {
     let ast = parse::parse(ts.clone().into());
-    let model = analyze::analyze(ast);
-    let ir = lower::lower(model);
+    let ir = lower::lower(ast);
     let _ = codegen::codegen(ir);
     TokenStream::new()
 }
